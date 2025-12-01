dba9cc8b907949dd703d3e51d6d8e7e3183048d6
Dominick Smith <dominick.smith938@gmail.com>
2025-12-01 14:04:15 -0600

    Close but not quite Day 01 - Part 2

--- 01 - Secret Entrance/01_Secret_Entrance.go
+++ 01 - Secret Entrance/01_Secret_Entrance.go
@@ -8,36 +8,62 @@ import (
 	"strconv"
 )
 
-func rotateLock(combo string, lockPOS int) int {
+func rotateLock(combo string, lockPOS int, passedZero int) (int, int) {
 	rotationDIR := combo[0]
-
 	comboToInt, _ := strconv.Atoi(combo[1:])
 
 	if rotationDIR == 'L' {
+		fmt.Println("Rotating Left: ", comboToInt, "CURRENT POS: ", lockPOS)
 		newLockPOS := lockPOS - comboToInt
+
+		if newLockPOS == 0 {
+			fmt.Println("HIT 0", passedZero)
+			passedZero += 1
+		}
+
 		if newLockPOS < 0 {
-			// fmt.Println("NEW LOCK POS", newLockPOS)
+			if len(combo[1:]) >= 3 {
+				passedZero += (1 * (comboToInt / 100))
+				fmt.Println("PASSED ZERO", (1 * (comboToInt / 100)), "Times.")
+			}
+			fmt.Println("PASSED ZERO", passedZero)
+			passedZero += 1
 			newLockPOS = newLockPOS % 100
 			newLockPOS += 100
 			newLockPOS = newLockPOS % 100
-			return newLockPOS
+			return newLockPOS, passedZero
 		}
-		return newLockPOS
+		return newLockPOS, passedZero
+
 	} else if rotationDIR == 'R' {
+		fmt.Println("Rotating Right: ", comboToInt, "CURRENT POS: ", lockPOS)
 		newLockPOS := lockPOS + comboToInt
-		if newLockPOS >= 100 {
-			newLockPOS = newLockPOS % 100
-			return newLockPOS
+
+		if newLockPOS == 0 {
+			fmt.Println("HIT 0", passedZero)
+			passedZero += 1
 		}
-		return newLockPOS
+
+		if newLockPOS >= 100 {
+			if len(combo[1:]) >= 3 {
+				passedZero += (1 * (comboToInt / 100))
+				fmt.Println("PASSED ZERO", (1 * (comboToInt / 100)), "Times.")
+			}
+			fmt.Println("PASSED ZERO", passedZero)
+			passedZero += 1
+			newLockPOS = newLockPOS % 100
+			return newLockPOS, passedZero
+		}
+		return newLockPOS, passedZero
 	}
-	return 0
+	return 0, 0
 }
 
 func main() {
 	var combos []string
 	lock := 50
 	zeroCount := 0
+	passedZero := 0
 
 	// Open combination file
 	f, err := os.Open("combination.txt")
@@ -54,11 +80,12 @@ func main() {
 	}
 
 	for i := 0; i < len(combos); i++ {
-		lock = rotateLock(combos[i], lock)
+		lock, passedZero = rotateLock(combos[i], lock, passedZero)
 		if lock == 0 {
 			zeroCount += 1
 		}
 	}
 
 	fmt.Println("zeroCount: ", zeroCount)
+	fmt.Println("passed zero: ", passedZero)
 }
